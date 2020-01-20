03d7093082757b4e5a95a8b4937294ac2a2f49d8
Jonathan Lee <li.junya@husky.neu.edu>
2020-01-19 22:12:56 -0500

    Lab1_2

---
+++ Lab_1_2/src/PersonalInfo/Person.java
@@ -0,0 +1,303 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PersonalInfo;
+
+/**
+ *
+ * @author junyaoli
+ */
+public class Person {
+    private String address;
+    private String city;
+    private String state;
+    private String zipcode;
+    
+    private String bankname1;
+    private String bankroutingnumber1;
+    private String bankaccountnumber1;
+    private String accountbalance1;
+    private String accounttype1;
+    
+    
+    private String bankname2;
+    private String bankroutingnumber2;
+    private String bankaccountnumber2;
+    private String accountbalance2;
+    private String accounttype2;
+    
+    private String firstname;
+    private String lastname;
+    private String phonenumber;
+    private String dateofbirth;
+    private String age;
+    private String height;
+    private String weight;
+    private String SSN;
+    
+    private String licensenumber;
+    private String issudate;
+    private String expireddate;
+    private String bloodtype;
+    private String picture;
+    
+    private String medicalrecordnumber;
+    private String alergy1;
+    private String alergy2;
+    private String alergy3;
+
+    public String getBankname1() {
+        return bankname1;
+    }
+
+    public void setBankname1(String bankname1) {
+        this.bankname1 = bankname1;
+    }
+
+    public String getBankroutingnumber1() {
+        return bankroutingnumber1;
+    }
+
+    public void setBankroutingnumber1(String bankroutingnumber1) {
+        this.bankroutingnumber1 = bankroutingnumber1;
+    }
+
+    public String getBankaccountnumber1() {
+        return bankaccountnumber1;
+    }
+
+    public void setBankaccountnumber1(String bankaccountnumber1) {
+        this.bankaccountnumber1 = bankaccountnumber1;
+    }
+
+    public String getAccountbalance1() {
+        return accountbalance1;
+    }
+
+    public void setAccountbalance1(String accountbalance1) {
+        this.accountbalance1 = accountbalance1;
+    }
+
+    public String getAccounttype1() {
+        return accounttype1;
+    }
+
+    public void setAccounttype1(String accounttype1) {
+        this.accounttype1 = accounttype1;
+    }
+
+    public String getBankname2() {
+        return bankname2;
+    }
+
+    public void setBankname2(String bankname2) {
+        this.bankname2 = bankname2;
+    }
+
+    public String getBankroutingnumber2() {
+        return bankroutingnumber2;
+    }
+
+    public void setBankroutingnumber2(String bankroutingnumber2) {
+        this.bankroutingnumber2 = bankroutingnumber2;
+    }
+
+    public String getBankaccountnumber2() {
+        return bankaccountnumber2;
+    }
+
+    public void setBankaccountnumber2(String bankaccountnumber2) {
+        this.bankaccountnumber2 = bankaccountnumber2;
+    }
+
+    public String getAccountbalance2() {
+        return accountbalance2;
+    }
+
+    public void setAccountbalance2(String accountbalance2) {
+        this.accountbalance2 = accountbalance2;
+    }
+
+    public String getAccounttype2() {
+        return accounttype2;
+    }
+
+    public void setAccounttype2(String accounttype2) {
+        this.accounttype2 = accounttype2;
+    }
+
+    
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getDateofbirth() {
+        return dateofbirth;
+    }
+
+    public void setDateofbirth(String dateofbirth) {
+        this.dateofbirth = dateofbirth;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getLicensenumber() {
+        return licensenumber;
+    }
+
+    public void setLicensenumber(String licensenumber) {
+        this.licensenumber = licensenumber;
+    }
+
+    public String getIssudate() {
+        return issudate;
+    }
+
+    public void setIssudate(String issudate) {
+        this.issudate = issudate;
+    }
+
+    public String getExpireddate() {
+        return expireddate;
+    }
+
+    public void setExpireddate(String expireddate) {
+        this.expireddate = expireddate;
+    }
+
+    public String getBloodtype() {
+        return bloodtype;
+    }
+
+    public void setBloodtype(String bloodtype) {
+        this.bloodtype = bloodtype;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getMedicalrecordnumber() {
+        return medicalrecordnumber;
+    }
+
+    public void setMedicalrecordnumber(String medicalrecordnumber) {
+        this.medicalrecordnumber = medicalrecordnumber;
+    }
+
+    public String getAlergy1() {
+        return alergy1;
+    }
+
+    public void setAlergy1(String alergy1) {
+        this.alergy1 = alergy1;
+    }
+
+    public String getAlergy2() {
+        return alergy2;
+    }
+
+    public void setAlergy2(String alergy2) {
+        this.alergy2 = alergy2;
+    }
+
+    public String getAlergy3() {
+        return alergy3;
+    }
+
+    public void setAlergy3(String alergy3) {
+        this.alergy3 = alergy3;
+    }
+    
+    
+}
---
+++ Lab_1_2/src/PersonalInfo/Photo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PersonalInfo;
+
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author junyaoli
+ */
+public class Photo {
+    String path;
+
+    
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
+
