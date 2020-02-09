51008562c8266ebe42f1e697a73feec4c441661f
Jonathan Lee <li.junya@husky.neu.edu>
2020-02-09 18:47:16 -0500

    Lab3

--- Lab3Skleton/src/Business/Product.java
+++ Lab3Skleton/src/Business/Product.java
@@ -14,6 +14,7 @@ public class Product {
     private double price;
     private int availNum;
     private String description;
+    private String nString;
     
     public String getName() {
         return name;
